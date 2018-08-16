bddbab09ade18203a4d30624c428bbd57a118164
croozer <croozerua@gmaail.com>
2018-08-17 00:04:53 +0300

    add

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url, include
-from rest_framework import routers
+from rest_framework import routers,urls
 from main.serializer import views
 
 router = routers.DefaultRouter()
